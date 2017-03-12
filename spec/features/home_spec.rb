429e2dbe216c60f5170ce15d46320db527a31544
Sean <sean@Seans-MacBook-Pro.local>
2017-03-12 16:47:39 -0400

    Clean up rspec.

--- spec/features/home_spec.rb
+++ spec/features/home_spec.rb
@@ -2,6 +2,11 @@ require 'rails_helper'
 
 feature 'the home page', type: :feature, js: true do
   before(:each) { visit root_path }
+  around(:each) do |example|
+    VCR.use_cassette('home') do
+      example.run
+    end
+  end
 
   scenario 'user can access homepage' do
     expect(page.status_code).to eq(200)
@@ -13,44 +18,33 @@ feature 'the home page', type: :feature, js: true do
   end
 
   scenario 'user enters query' do
-    VCR.use_cassette('home') do
-      fill_in 'q', with: 'Gehirn'
-      expect(page).to have_content('/ɡəˈhɪʁn/')
-      expect(page).to have_link('Gehirn')
-    end
+    fill_in 'q', with: 'Gehirn'
+    expect(page).to have_content('/ɡəˈhɪʁn/')
+    expect(page).to have_link('Gehirn')
   end
 
   scenario 'user enters off-case query' do
-    VCR.use_cassette('home') do
-      fill_in 'q', with: 'gehirn'
-      expect(page).to have_content('/ɡəˈhɪʁn/')
-      expect(page).to have_link('Gehirn')
-    end
+    fill_in 'q', with: 'gehirn'
+    expect(page).to have_content('/ɡəˈhɪʁn/')
+    expect(page).to have_link('Gehirn')
   end
 
   scenario 'no sound for query' do
-    VCR.use_cassette('home') do
-      fill_in 'q', with: 'Nichtraucherinnen'
-      expect(page).to have_content('/ˈnɪçtˌʀaʊ̯χəʀɪnən/')
-      expect(page).to have_content('No audio found.')
-    end
+    fill_in 'q', with: 'Nichtraucherinnen'
+    expect(page).to have_content('/ˈnɪçtˌʀaʊ̯χəʀɪnən/')
+    expect(page).to have_content('No audio found.')
   end
 
   scenario 'no result for query' do
-    VCR.use_cassette('home') do
-      fill_in 'q', with: 'Myfakeword'
-      expect(page).to have_content('No results.')
-    end
+    fill_in 'q', with: 'Myfakeword'
+    expect(page).to have_content('No results.')
   end
 
   scenario 'shows language-specific audio only' do
-    VCR.use_cassette('home') do
-      fill_in 'q', with: 'gift'
-      expect(page).to have_content('No audio found.')
-      fill_in 'q', with: 'Gift'
-      expect(find_link('Gift')[:href])
-        .to eq('http://upload.wikimedia.org/wikipedia/commons/0/01/De-Gift.ogg')
-    end
+    fill_in 'q', with: 'gift'
+    expect(page).to have_content('No audio found.')
+    fill_in 'q', with: 'Gift'
+    expect(find_link('Gift')[:href])
+      .to eq('http://upload.wikimedia.org/wikipedia/commons/0/01/De-Gift.ogg')
   end
-
 end
